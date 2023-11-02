0b2ff9eb4ca2df9676adf7f4028da9cba51b7245
Manik Deswal <manikdeswal@gmail.com>
2023-11-02 15:33:10 +0530

    View Entity add (Models)

---
+++ src/main/java/com/youtube/models/view.java
@@ -0,0 +1,38 @@
+package com.youtube.models;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "views")
+public class view {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private UUID id;
+
+    @NotNull
+    @Column(name="viewCount")
+    private Integer count;
+
+    public UUID getId(){
+        return id;
+    }
+    public Integer getViewCount(){
+        return count;
+    }
+
+    public void setViewCount(Integer count){
+        this.count=count;
+    }
+    
+}
